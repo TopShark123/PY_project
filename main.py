608fa78cb89d2cd17c039e9fcedb9be882a3318b
TopShark123 <vyotov07@gmail.com>
2023-05-21 21:14:33 +0300

    better player logic

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import math
 import pygame
 from os import listdir
 from os.path import isfile, join
-import Player
 pygame.init()
 
 pygame.display.set_caption("Platformer")
@@ -13,9 +12,43 @@ WIDTH, HEIGHT = 800, 600
 FPS = 60
 PLAYER_VEL = 5
 window = pygame.display.set_mode((WIDTH, HEIGHT))
+COLOR = (255,0,0)
 
 
+class Player(pygame.sprite.Sprite):
+    
 
+    def __init__(self, x,y,width,height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        self.direction = "left"
+        self.animation_count = 0
+        self.fall_count = 0
+        self.jump_count = 0
+        self.hit = False
+        self.hit_count = 0
+
+    def move_left(self, vel):
+        self.x_vel = -vel
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
+
+    def move_right(self, vel):
+        self.x_vel = vel
+        if self.direction != "right":
+            self.direction = "right"
+            self.animation_count = 0
+
+    
+    def loop(self, fps):
+        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
+        self.move(self.x_vel, self.y_vel)
+
+    def draw(self,win):
+        pygame.draw.rect(win, COLOR, self.rect)
 
 def get_background(name):
     image = pygame.image.load(join("assets","Background",name))
@@ -29,18 +62,20 @@ def get_background(name):
 
     return tiles,image
 
-def draw(window, background, bg_image):
+def draw(window, background, bg_image,player):
     for tile in background:
         window.blit(bg_image,tile)
 
-
-
+    player.draw(window)
+    
     pygame.display.update()
 
 
 def main(window):
     clock = pygame.time.Clock()
     background, bg_color= get_background("Blue.png")
+
+    player = Player(100,100,50,50)
     run = True
     while run:
         clock.tick(FPS)
@@ -50,7 +85,7 @@ def main(window):
                 run = False
                 break
 
-        draw(window, background, bg_color)
+        draw(window, background, bg_color,player)
     pygame.quit()
     quit()
 
