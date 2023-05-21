56ba4a6050e6db9eb68e40d3e0a3eb66620adaaa
TopShark123 <vyotov07@gmail.com>
2023-05-21 20:59:16 +0300

    creating the player

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import math
 import pygame
 from os import listdir
 from os.path import isfile, join
+import Player
 pygame.init()
 
 pygame.display.set_caption("Platformer")
@@ -15,8 +16,31 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
 
+
+def get_background(name):
+    image = pygame.image.load(join("assets","Background",name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH// width +1):
+        for j in range(HEIGHT // height +1):
+            pos = (i* width, j*height)
+            tiles.append(pos)
+
+    return tiles,image
+
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_image,tile)
+
+
+
+    pygame.display.update()
+
+
 def main(window):
     clock = pygame.time.Clock()
+    background, bg_color= get_background("Blue.png")
     run = True
     while run:
         clock.tick(FPS)
@@ -26,7 +50,7 @@ def main(window):
                 run = False
                 break
 
-
+        draw(window, background, bg_color)
     pygame.quit()
     quit()
 
