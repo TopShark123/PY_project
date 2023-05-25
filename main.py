e59d6e4c2b9dc10d8c8de249dba1c0adf1aa9f1c
TopShark123 <vyotov07@gmail.com>
2023-05-25 10:14:28 +0300

    creating block class

--- main.py
+++ main.py
@@ -119,7 +119,17 @@ class Object(pygame.sprite.Sprite):
         win.blit(self.image, (self.rect.x - offset_x, self.rect.y))
 
 
-#-------------------------------------------------------------------------------------------------   
+#-------------------------------------------------------------------------------------------------
+
+
+class Block(Object):
+    def __init__(self, x, y, size):
+        super().__init__(x, y, size, size)
+        block = get_block(size)
+        self.image.blit(block, (0, 0))
+        self.mask = pygame.mask.from_surface(self.image)
+
+#------------------------------------------------------------------------------------
 def get_background(name):
     image = pygame.image.load(join("assets","Background",name))
     _, _, width, height = image.get_rect()
