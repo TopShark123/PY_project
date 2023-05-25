dc076907112e609ec27c3a7a40e5ec4c8e835de2
TopShark123 <vyotov07@gmail.com>
2023-05-25 11:36:58 +0300

    adding offset_x to the functions

--- main.py
+++ main.py
@@ -179,14 +179,14 @@ def get_background(name):
 
     return tiles,image
 
-def draw(window, background, bg_image, player, objects):
+def draw(window, background, bg_image, player, objects,offset_x):
     for tile in background:
         window.blit(bg_image, tile)
 
     for obj in objects:
-        obj.draw(window)
+        obj.draw(window,offset_x)
 
-    player.draw(window)
+    player.draw(window,offset_x)
 
     pygame.display.update()
 
