1312ad088ac42db7887284f756d0e9de070e91c3
TopShark123 <vyotov07@gmail.com>
2023-05-25 10:43:36 +0300

    starting the collision functions

--- main.py
+++ main.py
@@ -171,7 +171,21 @@ def handle_move(player):
     if keys[pygame.K_RIGHT]:
         player.move_right(PLAYER_VEL)
 
-  
+def handle_vertical_collision(player, objects, dy):
+    collided_objects = []
+    for obj in objects:
+        if pygame.sprite.collide_mask(player, obj):
+            if dy > 0:
+                player.rect.bottom = obj.rect.top
+                player.landed()
+            elif dy < 0:
+                player.rect.top = obj.rect.bottom
+                player.hit_head()
+
+            collided_objects.append(obj)
+
+    return collided_objects
+
 
 def main(window):
     clock = pygame.time.Clock()
