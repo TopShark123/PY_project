654d5bfde001c91388cb380896adec83a90132d5
TopShark123 <vyotov07@gmail.com>
2023-05-26 09:34:56 +0300

    adding horizontal collision

--- main.py
+++ main.py
@@ -219,6 +219,19 @@ def handle_vertical_collision(player, objects, dy):
     return collided_objects
 
 
+def collide(player, objects, dx):
+    player.move(dx, 0)
+    player.update()
+    collided_object = None
+    for obj in objects:
+        if pygame.sprite.collide_mask(player, obj):
+            collided_object = obj
+            break
+
+    player.move(-dx, 0)
+    player.update()
+    return collided_object
+
 def main(window):
     clock = pygame.time.Clock()
     background, bg_color= get_background("Green.png")
