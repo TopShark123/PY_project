45ade4f8998fff7b83b256cd2b3285714f21aa71
TopShark123 <vyotov07@gmail.com>
2023-05-26 09:41:11 +0300

    fire loop

--- main.py
+++ main.py
@@ -185,6 +185,20 @@ class Fire(Object):
     def off(self):
         self.animation_name = "off"
 
+    
+    def loop(self):
+        sprites = self.fire[self.animation_name]
+        sprite_index = (self.animation_count //
+                        self.ANIMATION_DELAY) % len(sprites)
+        self.image = sprites[sprite_index]
+        self.animation_count += 1
+
+        self.rect = self.image.get_rect(topleft=(self.rect.x, self.rect.y))
+        self.mask = pygame.mask.from_surface(self.image)
+
+        if self.animation_count // self.ANIMATION_DELAY > len(sprites):
+            self.animation_count = 0
+
 #--------------------------------------------------------------------------------------------
 def get_background(name):
     image = pygame.image.load(join("assets","Background",name))
